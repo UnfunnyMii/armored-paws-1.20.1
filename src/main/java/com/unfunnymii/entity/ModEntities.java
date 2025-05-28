7d4d9b2573a14741d22fc7aaeeb9677e53061667
UnfunnyMii <unfunnymii@gmail.com>
2025-05-28 12:58:01 -0400

    Initial commit

---
+++ src/main/java/com/unfunnymii/entity/ModEntities.java
@@ -0,0 +1,23 @@
+package com.unfunnymii.entity;
+
+import com.unfunnymii.ArmadilloBackport;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModEntities {
+    public static final EntityType<ArmadilloEntity> ARMADILLO = Registry.register(Registries.ENTITY_TYPE,
+            new Identifier(ArmadilloBackport.MOD_ID, "armadillo"),
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, ArmadilloEntity::new)
+                    .dimensions(EntityDimensions.fixed(1f, 1f)).build());
+
+
+
+    public static void init() {
+        ArmadilloBackport.LOGGER.info("Registering Entities for " + ArmadilloBackport.MOD_ID);
+    }
+}
